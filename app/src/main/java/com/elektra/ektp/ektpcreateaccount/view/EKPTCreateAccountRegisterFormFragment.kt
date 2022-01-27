0690c60cf300cf08d4e793613076bff36b7f906e
diegovazpez <diego.vazpez@hotmail.com>
2022-01-27 15:38:23 -0600

    Se agrega databinding en register form

--- app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKPTCreateAccountRegisterFormFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKPTCreateAccountRegisterFormFragment.kt
@@ -5,20 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.FragmentEkptCreateAccountRegisterFormBinding
 
 class EKPTCreateAccountRegisterFormFragment : Fragment() {
+
+    private lateinit var binding: FragmentEkptCreateAccountRegisterFormBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(
-            R.layout.fragment_ekpt_create_account_register_form,
-            container,
-            false
-        )
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_ekpt_create_account_register_form, container, false)
+
+        return binding.root
     }
 
 }
