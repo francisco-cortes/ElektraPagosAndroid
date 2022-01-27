85886193b78f1d6348946c70b4fc36885edeec62
diegovazpez <diego.vazpez@hotmail.com>
2022-01-27 15:25:01 -0600

    Se ajusta pantalla fragment_ektp_create_account_sms_verification.xml de acuerdo con prototipo XD

--- app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKTPCreateAccountSMSVerificationFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKTPCreateAccountSMSVerificationFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.FragmentEktpCreateAccountSmsVerificationBinding
 
 class EKTPCreateAccountSMSVerificationFragment : Fragment() {
 
@@ -14,11 +16,10 @@ class EKTPCreateAccountSMSVerificationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(
-            R.layout.fragment_ektp_create_account_sms_verification,
-            container,
-            false
-        )
+        val binding = DataBindingUtil.inflate<FragmentEktpCreateAccountSmsVerificationBinding>(inflater,
+            R.layout.fragment_ektp_create_account_sms_verification, container, false)
+
+        return  binding.root
     }
 
 }
