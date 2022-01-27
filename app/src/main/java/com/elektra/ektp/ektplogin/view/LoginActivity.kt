78653434a3556bf98823a6742bc140ac745220d3
francisco <fj23pk@hotmail.com>
2022-01-27 11:08:36 -0600

    login activity creado

---
+++ app/src/main/java/com/elektra/ektp/ektplogin/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.elektra.ektp.ektplogin.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elektra.ektp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
