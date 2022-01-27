c5385698a4e8f34bcff669d607b23128c42b4a38
francisco <fj23pk@hotmail.com>
2022-01-27 11:32:01 -0600

    binding setccontetview añadido al Loginactivity.kt

--- app/src/main/java/com/elektra/ektp/ektplogin/view/LoginActivity.kt
+++ app/src/main/java/com/elektra/ektp/ektplogin/view/LoginActivity.kt
@@ -2,11 +2,16 @@ package com.elektra.ektp.ektplogin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView<ActivityLoginBinding>(this,R.layout.activity_login)
     }
 }
