b91264ba1a7826cf37394e2adbeaee305786c253
diegovazpez <diego.vazpez@hotmail.com>
2022-01-27 16:21:54 -0600

    Se crea fragmento de creación de cuenta exitoso vacío

---
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKTPCreateAccountSuccessfulFragment.kt
@@ -0,0 +1,20 @@
+package com.elektra.ektp.ektpcreateaccount.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.elektra.ektp.R
+
+class EKTPCreateAccountSuccessfulFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ektp_create_account_successful, container, false)
+    }
+
+}
