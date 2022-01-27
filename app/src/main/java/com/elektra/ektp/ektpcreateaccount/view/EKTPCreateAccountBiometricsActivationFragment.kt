0cc20ea15aff9968fa52fbaf036f54d653577cab
diegovazpez <diego.vazpez@hotmail.com>
2022-01-27 16:10:30 -0600

    Se agrega databinding al fragmento activación de biometricos

--- app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKTPCreateAccountBiometricsActivationFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKTPCreateAccountBiometricsActivationFragment.kt
@@ -5,20 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.FragmentEktpCreateAccountBiometricsActivationBinding
 
 class EKTPCreateAccountBiometricsActivationFragment : Fragment() {
+
+    private lateinit var binding : FragmentEktpCreateAccountBiometricsActivationBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(
-            R.layout.fragment_ektp_create_account_biometrics_activation,
-            container,
-            false
-        )
+        binding = DataBindingUtil.inflate<FragmentEktpCreateAccountBiometricsActivationBinding>(inflater,
+            R.layout.fragment_ektp_create_account_biometrics_activation, container, false)
+
+        return binding.root
     }
 
 }
