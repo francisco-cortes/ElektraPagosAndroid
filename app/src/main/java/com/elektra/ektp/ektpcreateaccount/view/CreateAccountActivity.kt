11d0d9ff5abb0f272657350ae0f447c706421213
diegovazpez <diego.vazpez@hotmail.com>
2022-01-27 14:03:50 -0600

    Se crea CreateAccountActivity vacía

---
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/CreateAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.elektra.ektp.ektpcreateaccount.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elektra.ektp.R
+
+class CreateAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
