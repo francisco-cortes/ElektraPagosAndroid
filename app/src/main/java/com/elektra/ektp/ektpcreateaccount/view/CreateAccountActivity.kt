e3fda598ec94446dd7b3acbd344a6fd567b3eb23
diegovazpez <diego.vazpez@hotmail.com>
2022-01-27 14:14:59 -0600

    Se agrega función DataBinding en CreateAccountActivity

--- app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/CreateAccountActivity.kt
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/CreateAccountActivity.kt
@@ -2,11 +2,22 @@ package com.elektra.ektp.ektpcreateaccount.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.ActivityCreateAccountBinding
 
 class CreateAccountActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreateAccountBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_account)
+        @Suppress("UNUSED_VARIABLE")
+        binding = DataBindingUtil.setContentView<ActivityCreateAccountBinding>(this, R.layout.activity_create_account)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
     }
 }
