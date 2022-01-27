3d93b1f0933c62bf74aa00af1e50549c4eaf274c
diegovazpez <diego.vazpez@hotmail.com>
2022-01-27 16:00:30 -0600

    Se agrega databinding al fragmento crear contraseña

--- app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKTPCreateAccountCreatePassFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKTPCreateAccountCreatePassFragment.kt
@@ -5,16 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.FragmentEktpCreateAccountCreatePassBinding
 
 class EKTPCreateAccountCreatePassFragment : Fragment() {
+
+    private lateinit var binding: FragmentEktpCreateAccountCreatePassBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ektp_create_account_create_pass, container, false)
+        binding = DataBindingUtil.inflate<FragmentEktpCreateAccountCreatePassBinding>(inflater,
+            R.layout.fragment_ektp_create_account_create_pass, container, false)
+
+        return binding.root
     }
 
 }
