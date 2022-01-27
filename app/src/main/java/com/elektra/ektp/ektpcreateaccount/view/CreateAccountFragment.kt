1458fca00e2bdb71e08d5f7e0f86242b0519d4e1
diegovazpez <diego.vazpez@hotmail.com>
2022-01-27 15:02:46 -0600

    Se agrega inflater para Fragment Create Account

--- app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/CreateAccountFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/CreateAccountFragment.kt
@@ -5,16 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.FragmentCreateAccountBinding
 
 class CreateAccountFragment : Fragment() {
+
+    private lateinit var binding: FragmentCreateAccountBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_account, container, false)
+        binding = DataBindingUtil.inflate<FragmentCreateAccountBinding>(inflater,
+            R.layout.fragment_create_account, container, false)
+
+        return binding.root
     }
 
 }
