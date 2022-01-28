67fd7b5a76780cb7380005210f6a4371884dc0c2
francisco <fj23pk@hotmail.com>
2022-01-27 19:30:19 -0600

    databindingview para el activity login

--- app/src/main/java/com/elektra/ektp/ektphome/view/EKTPHomeActivity.kt
+++ app/src/main/java/com/elektra/ektp/ektphome/view/EKTPHomeActivity.kt
@@ -2,11 +2,14 @@ package com.elektra.ektp.ektphome.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.ActivityLoginBinding
 
 class EKTPHomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ektphome)
+        binding = DataBindingUtil.setContentView<ActivityLoginBinding>(this,R.layout.activity_ektphome)
     }
 }
