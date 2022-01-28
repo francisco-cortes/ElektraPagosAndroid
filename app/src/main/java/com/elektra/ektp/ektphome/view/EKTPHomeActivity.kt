d9119001ec1f181ea76b8addad419d52587ea1a0
francisco <fj23pk@hotmail.com>
2022-01-28 14:30:01 -0600

    fragment repacer un funcion de activity home

--- app/src/main/java/com/elektra/ektp/ektphome/view/EKTPHomeActivity.kt
+++ app/src/main/java/com/elektra/ektp/ektphome/view/EKTPHomeActivity.kt
@@ -3,6 +3,7 @@ package com.elektra.ektp.ektphome.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.elektra.ektp.R
 import com.elektra.ektp.databinding.ActivityLoginBinding
 
@@ -11,5 +12,12 @@ class EKTPHomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView<ActivityLoginBinding>(this,R.layout.activity_ektphome)
+    }
+
+    private fun openFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.homeNavHostFragment, fragment)
+            commit()
+        }
     }
 }
