2e327d703ac83df2cfe121d271da2c2491f9c073
francisco <fj23pk@hotmail.com>
2022-01-28 14:41:19 -0600

    bug de home databinding arreglado y fragmento en blanco añadido

--- app/src/main/java/com/elektra/ektp/ektphome/view/EKTPHomeActivity.kt
+++ app/src/main/java/com/elektra/ektp/ektphome/view/EKTPHomeActivity.kt
@@ -5,13 +5,46 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.ActivityEktphomeBinding
 import com.elektra.ektp.databinding.ActivityLoginBinding
 
 class EKTPHomeActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: ActivityEktphomeBinding
+
+    private var isHome = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView<ActivityLoginBinding>(this,R.layout.activity_ektphome)
+        binding = DataBindingUtil.setContentView<ActivityEktphomeBinding>(this,R.layout.activity_ektphome)
+
+        openFragment(EKTPHomeMainFragment())
+
+        /*binding.navbar.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId){
+                R.id.startButton->{
+                    openFragment(HomeMainFragment())
+                    isHome = true
+                    true
+                }
+                R.id.profileButton->{
+                    openFragment(HomeProfileFragment())
+                    isHome = false
+                    true
+                }
+                R.id.noticeButton->{
+                    openFragment(HomeNoticeFragment())
+                    isHome = false
+                    true
+                }
+                R.id.settingsButton->{
+                    openFragment(HomeSettingFragment())
+                    isHome = false
+                    true
+                }
+                else -> false
+
+            }
+        }*/
     }
 
     private fun openFragment(fragment: Fragment){
