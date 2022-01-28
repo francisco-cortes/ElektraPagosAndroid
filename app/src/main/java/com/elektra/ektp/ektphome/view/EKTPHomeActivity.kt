d3e8cc374c91485addc89b5876656b2ec4d91854
francisco <fj23pk@hotmail.com>
2022-01-27 19:17:28 -0600

    activity vacio creado para flujo de home

---
+++ app/src/main/java/com/elektra/ektp/ektphome/view/EKTPHomeActivity.kt
@@ -0,0 +1,12 @@
+package com.elektra.ektp.ektphome.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elektra.ektp.R
+
+class EKTPHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ektphome)
+    }
+}
