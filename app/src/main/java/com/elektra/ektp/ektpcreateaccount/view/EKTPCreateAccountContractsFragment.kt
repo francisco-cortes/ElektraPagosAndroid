b10516d83cc701d9b24c1030f0f753598c6de482
diegovazpez <diego.vazpez@hotmail.com>
2022-01-31 13:53:25 -0600

    Se agregan funciones SetOnCLickListener para contratos TextView

--- app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKTPCreateAccountContractsFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKTPCreateAccountContractsFragment.kt
@@ -1,11 +1,13 @@
 package com.elektra.ektp.ektpcreateaccount.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.elektra.ektp.R
 import com.elektra.ektp.databinding.FragmentEktpCreateAccountContractsBinding
 
@@ -20,6 +22,40 @@ class EKTPCreateAccountContractsFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate<FragmentEktpCreateAccountContractsBinding>(inflater,
             R.layout.fragment_ektp_create_account_contracts, container, false)
+
+        binding.termsContinueButton.isEnabled = false
+
+        binding.term1TextView.setOnClickListener { view: View ->
+
+        }
+
+        binding.term2TextView.setOnClickListener { view: View ->
+
+        }
+
+        binding.term3TextView.setOnClickListener { view: View ->
+
+        }
+
+        binding.term4TextView.setOnClickListener { view: View ->
+
+        }
+
+        binding.term5TextView.setOnClickListener { view: View ->
+
+        }
+
+        binding.term6TextView.setOnClickListener { view: View ->
+
+        }
+
+        binding.term7TextView.setOnClickListener { view: View ->
+
+        }
+
+        binding.term8TextView.setOnClickListener { view: View ->
+            
+        }
 
         return binding.root
     }
