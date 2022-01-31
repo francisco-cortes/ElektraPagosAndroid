18a572d01bee4abfd4fe6655782cbbd8bcfea09a
diegovazpez <diego.vazpez@hotmail.com>
2022-01-31 13:45:11 -0600

    Se agrega databinding a ContractsView

--- app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKTPCreateAccountContractsViewFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKTPCreateAccountContractsViewFragment.kt
@@ -5,19 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.FragmentEktpCreateAccountContractsViewBinding
 
 class EKTPCreateAccountContractsViewFragment : Fragment() {
+
+    private lateinit var binding: FragmentEktpCreateAccountContractsViewBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(
-            R.layout.fragment_ektp_create_account_contracts_view,
-            container,
-            false
-        )
+        binding = DataBindingUtil.inflate<FragmentEktpCreateAccountContractsViewBinding>(inflater,
+        R.layout.fragment_ektp_create_account_contracts_view, container, false)
+
+        return binding.root
     }
 }
