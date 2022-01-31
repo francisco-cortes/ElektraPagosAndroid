51726f50d8f8f5a673eb221ddf92cb71daad1292
francisco <fj23pk@hotmail.com>
2022-01-31 10:53:14 -0600

    binding para  helpsettings

--- app/src/main/java/com/elektra/ektp/ektphome/view/EKTPHomeSettingsHelpActivity.kt
+++ app/src/main/java/com/elektra/ektp/ektphome/view/EKTPHomeSettingsHelpActivity.kt
@@ -2,11 +2,14 @@ package com.elektra.ektp.ektphome.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.ActivityEktphomeSettingsHelpBinding
 
 class EKTPHomeSettingsHelpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEktphomeSettingsHelpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ektphome_settings_help)
+        binding = DataBindingUtil.setContentView<ActivityEktphomeSettingsHelpBinding>(this,R.layout.activity_ektphome_settings_help)
     }
 }
