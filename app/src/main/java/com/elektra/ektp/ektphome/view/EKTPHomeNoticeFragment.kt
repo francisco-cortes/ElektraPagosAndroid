8442df519351946712d6b14aace794c712e4b825
francisco <fj23pk@hotmail.com>
2022-01-30 22:34:02 -0600

    databindin inflater implementado par el fragmento notificaciones

--- app/src/main/java/com/elektra/ektp/ektphome/view/EKTPHomeNoticeFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektphome/view/EKTPHomeNoticeFragment.kt
@@ -5,15 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.FragmentEktpHomeNoticeBinding
 
 class EKTPHomeNoticeFragment : Fragment() {
+
+    private lateinit var binding : FragmentEktpHomeNoticeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_ektp_home_notice, container, false)
+        binding = DataBindingUtil.inflate<FragmentEktpHomeNoticeBinding>(inflater,R.layout.fragment_ektp_home_notice, container, false)
+        return binding.root
     }
 
 }
