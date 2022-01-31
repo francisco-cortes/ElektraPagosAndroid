d7b4b408b0fd9ec2a7471bf3ee934e61d8caf0fe
francisco <fj23pk@hotmail.com>
2022-01-31 10:47:49 -0600

    widgets añadidos a home help

---
+++ app/src/main/java/com/elektra/ektp/ektphome/view/EKTPHomeSettingsHelpActivity.kt
@@ -0,0 +1,12 @@
+package com.elektra.ektp.ektphome.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elektra.ektp.R
+
+class EKTPHomeSettingsHelpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ektphome_settings_help)
+    }
+}
