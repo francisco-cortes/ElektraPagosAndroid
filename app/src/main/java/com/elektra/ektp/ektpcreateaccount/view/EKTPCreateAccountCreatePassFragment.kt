58ec66d7971ba86ea136c81dd5444250a47fd3f2
diegovazpez <diego.vazpez@hotmail.com>
2022-01-31 17:38:24 -0600

    Se agrega funcion TextWatcher para validación de contraseña con UserValidations

--- app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKTPCreateAccountCreatePassFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKTPCreateAccountCreatePassFragment.kt
@@ -1,6 +1,8 @@
 package com.elektra.ektp.ektpcreateaccount.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -32,6 +34,77 @@ class EKTPCreateAccountCreatePassFragment : Fragment() {
 
         binding.button7.isEnabled = false
         binding.matchPass.isVisible = false
+
+        binding.insertPass.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                passTextVar = s.toString()
+                if(passTextVar.length < 8 ){
+                    binding.notMinIcon.isVisible = true
+                    binding.okMinIcon.isVisible = false
+                    binding.button7.isEnabled = false
+                }
+                else{
+                    binding.okMinIcon.isVisible = true
+                    binding.notMinIcon.isVisible = false
+                }
+                if(passTextVar.length in 8..14 ){
+                    binding.okMaxIcon.isVisible = true
+                    binding.notMaxIcon.isVisible = false
+                }
+                else{
+                    binding.okMaxIcon.isVisible = false
+                    binding.notMaxIcon.isVisible = true
+                    binding.button7.isEnabled = false
+                }
+                if (validations.checkRepeatedChars(passTextVar)){
+                    binding.okRepeatedIcon.isVisible = true
+                    binding.notRepeatedIcon.isVisible = false
+                }
+                else{
+                    binding.okRepeatedIcon.isVisible = false
+                    binding.notRepeatedIcon.isVisible = true
+                    binding.button7.isEnabled = false
+                }
+                if (validations.checkBankString(passTextVar)){
+                    binding.okNameIcon.isVisible = true
+                    binding.notNameIcon.isVisible = false
+                }
+                else{
+                    binding.okNameIcon.isVisible = false
+                    binding.notNameIcon.isVisible = true
+                    binding.button7.isEnabled = false
+                }
+                if (validations.checkConsecutiveString(passTextVar)){
+                    binding.okMConsecutiveIcon.isVisible = true
+                    binding.notConsecutiveIcon.isVisible = false
+                }
+                else{
+                    binding.okMConsecutiveIcon.isVisible = false
+                    binding.notConsecutiveIcon.isVisible = true
+                    binding.button7.isEnabled = false
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                passTextVar = s.toString()
+                if (passTextVar.length in 8..14){
+                    if (passTextVar != passTextVar2){
+                        binding.notMatchesIcon.isVisible = true
+                        binding.matchPass.isVisible = true
+                        binding.button7.isEnabled = false
+                    }
+                    else{
+                        binding.notMatchesIcon.isVisible = false
+                        binding.matchPass.isVisible = false
+                        binding.button7.isEnabled = true
+                    }
+                }
+            }
+        })
 
         return binding.root
     }
