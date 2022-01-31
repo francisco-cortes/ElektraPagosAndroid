02ab7e3e3b1cdf7a2dc82958aa7713bb8b12a048
francisco <fj23pk@hotmail.com>
2022-01-31 10:05:24 -0600

    binding para el fragment settings

--- app/src/main/java/com/elektra/ektp/ektphome/view/EKTPHomeSettingsFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektphome/view/EKTPHomeSettingsFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.FragmentEktpHomeSettingsBinding
 
 class EKTPHomeSettingsFragment : Fragment() {
+
+    private lateinit var binding: FragmentEktpHomeSettingsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,7 +19,9 @@ class EKTPHomeSettingsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
-        return inflater.inflate(R.layout.fragment_ektp_home_settings, container, false)
+        binding = DataBindingUtil.inflate<FragmentEktpHomeSettingsBinding>(inflater,R.layout.fragment_ektp_home_settings, container, false)
+
+        return binding.root
     }
 
 }
