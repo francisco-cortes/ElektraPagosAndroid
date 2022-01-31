750f22d02ff6c3f02c52a84fa22b764ae2df1e17
diegovazpez <diego.vazpez@hotmail.com>
2022-01-31 11:54:31 -0600

    Se agrega función OnFocusChangeLstener con ViewModel en Street (el anterior commit en realidad fue TextWatcher)

--- app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKPTCreateAccountRegisterFormFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKPTCreateAccountRegisterFormFragment.kt
@@ -321,6 +321,35 @@ class EKPTCreateAccountRegisterFormFragment : Fragment() {
 
         })
 
+        binding.insertStreet.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                if (validations.checkValidInput(streetUser) && streetFlag == 0){
+                    streetFlag = 1
+                    progressValue += 10
+                    completed += 1
+                    binding.button5.isEnabled =
+                        validations.checkFieldsProgressBar(
+                            zipCode,
+                            colonyUser,
+                            streetUser,
+                            exteriorNumber,
+                            country,
+                            state,
+                            town,
+                            completed
+                        )
+                }
+                else{
+                    if (!validations.checkValidInput(streetUser) && streetFlag ==1){
+                        streetFlag = 0
+                        progressValue -= 10
+                        completed -= 1
+                    }
+                }
+                progressInForm(progressValue, completed)
+            }
+        }
+
 
         return binding.root
     }
