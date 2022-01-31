80f131fd77dbc62c6b0a250bddc58dcd6183532c
diegovazpez <diego.vazpez@hotmail.com>
2022-01-31 12:31:19 -0600

    Se agrega función TextWatcher para InteriorNumber

--- app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKPTCreateAccountRegisterFormFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKPTCreateAccountRegisterFormFragment.kt
@@ -444,6 +444,29 @@ class EKPTCreateAccountRegisterFormFragment : Fragment() {
             }
         }
 
+        binding.interiorNumber.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                interiorNumber = s.toString()
+                if (validations.checkValidInput(interiorNumber) || interiorNumber.isEmpty()){
+                    binding.interiorNumber.setBackgroundResource(R.drawable.rounded_rectangle_gray)
+                    binding.invalidInteriorText.isVisible = false
+                }
+                else{
+                    binding.interiorNumber.setBackgroundResource(R.drawable.validation_edit_text)
+                    binding.invalidInteriorText.isVisible = true
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+
 
         return binding.root
     }
