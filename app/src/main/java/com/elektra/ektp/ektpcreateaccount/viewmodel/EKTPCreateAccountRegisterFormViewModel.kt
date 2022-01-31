d01dc7e1e655b388884ca8ca6cd8588148e141e9
diegovazpez <diego.vazpez@hotmail.com>
2022-01-31 10:12:10 -0600

    Se agrega función ViewModel para obtener datos de SharedPreferences en RegisterFormViewModel

--- app/src/main/java/com/elektra/ektp/ektpcreateaccount/viewmodel/EKTPCreateAccountRegisterFormViewModel.kt
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/viewmodel/EKTPCreateAccountRegisterFormViewModel.kt
@@ -1,6 +1,20 @@
 package com.elektra.ektp.ektpcreateaccount.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.elektra.ektp.ektpsharedpreferences.EKPTUserApplication.Companion.preferences
+import com.elektra.ektp.ektpsharedpreferences.EKTPUserPreferences
 
 class EKTPCreateAccountRegisterFormViewModel: ViewModel() {
+
+    fun getSavedRegisterData(): Array<String>
+    {
+        val userData: Array<String> = emptyArray<String>()
+        userData[0] = preferences.getNameUser()
+        userData[1] = preferences.getPaternalUser()
+        userData[2] = preferences.getMaternalUser()
+        userData[3] = preferences.getBirthDateUser()
+        userData[4] = preferences.getBirthSiteUser()
+        userData[5] = preferences.getGenderUser()
+        return userData
+    }
 }
