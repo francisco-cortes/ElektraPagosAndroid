9ec81d335e722234419d75b4d9cff7936fcf691f
diegovazpez <diego.vazpez@hotmail.com>
2022-01-30 19:11:51 -0600

    Se agregan funciones de validación de CódigoSMS en ViewModel

--- app/src/main/java/com/elektra/ektp/ektpcreateaccount/viewmodel/EKTPCreateAccountSMSVerificationViewModel.kt
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/viewmodel/EKTPCreateAccountSMSVerificationViewModel.kt
@@ -4,5 +4,13 @@ import androidx.lifecycle.ViewModel
 
 class EKTPCreateAccountSMSVerificationViewModel: ViewModel() {
 
-    
+    fun concatenaterCode(codeChar1: String, codeChar2: String, codeChar3: String, codeChar4: String, codeChar5: String): String {
+        var codeString = ""
+        codeString = codeChar1 + codeChar2 + codeChar3 + codeChar4 + codeChar5
+        return codeString
+    }
+
+    fun codeLenghtChecker(codeString: String): Boolean {
+        return codeString.length == 5
+    }
 }
