45abe6fbebbb4d1e5eb657da0bd60603547648f5
diegovazpez <diego.vazpez@hotmail.com>
2022-02-01 16:31:07 -0600

    Se agregan variables globales en ForgottenPassAuthorizationCodeFragment

--- app/src/main/java/com/elektra/ektp/ektpforgottenpass/view/EKTPForgottenPassAuthorizationCodeFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpforgottenpass/view/EKTPForgottenPassAuthorizationCodeFragment.kt
@@ -11,11 +11,20 @@ import com.elektra.ektp.R
 import com.elektra.ektp.databinding.FragmentEktpForgottenPassAuthorizationCodeBinding
 import com.elektra.ektp.databinding.FragmentEktpForgottenPassPhoneNumberBinding
 import com.elektra.ektp.ektpforgottenpass.view.viewmodel.EKTPForgottenPassAuthorizationCodeViewModel
+import com.elektra.ektp.uservalidations.UserValidations
 
 class EKTPForgottenPassAuthorizationCodeFragment : Fragment() {
 
     private lateinit var binding: FragmentEktpForgottenPassAuthorizationCodeBinding
     private val authorizationViewModel: EKTPForgottenPassAuthorizationCodeViewModel by viewModels()
+    val validations = UserValidations()
+
+    private lateinit var codeSMS: String
+    private var codechar1 = ""
+    private var codechar2 = ""
+    private var codechar3 = ""
+    private var codechar4 = ""
+    private var codechar5 = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +33,8 @@ class EKTPForgottenPassAuthorizationCodeFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater,
         R.layout.fragment_ektp_forgotten_pass_authorization_code, container, false)
+
+        binding.buttonAuth.isEnabled = false
 
         return binding.root
     }
