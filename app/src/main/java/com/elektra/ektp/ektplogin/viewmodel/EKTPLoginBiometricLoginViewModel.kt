b2230a6f6bb3a52cda44a44d1b12294286b31094
Francisco Javier Cortes Torres <fcortest@EKT1779608FRANCISCOMACLAP.local>
2022-02-01 13:15:26 -0600

    viewmodel para el logincon biometrico, funciones para obtener datos del sharedpreferences

--- app/src/main/java/com/elektra/ektp/ektplogin/viewmodel/EKTPLoginBiometricLoginViewModel.kt
+++ app/src/main/java/com/elektra/ektp/ektplogin/viewmodel/EKTPLoginBiometricLoginViewModel.kt
@@ -1,8 +1,17 @@
 package com.elektra.ektp.ektplogin.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.elektra.ektp.ektpsharedpreferences.EKPTUserApplication
 
 class EKTPLoginBiometricLoginViewModel : ViewModel() {
 
-    
+    fun getSavedDataLogin() : ArrayList<String>{
+        val loginData = arrayListOf<String>()
+
+        loginData.add(EKPTUserApplication.preferences.getBioStatus().toString())
+        loginData.add(EKPTUserApplication.preferences.getBioType().toString())
+        loginData.add(EKPTUserApplication.preferences.getEncryptToken())
+
+        return  loginData
+    }
 }
