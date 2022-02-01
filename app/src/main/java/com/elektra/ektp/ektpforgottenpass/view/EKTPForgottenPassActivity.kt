5fad5236b9bc0e4c28125dc6a0a9c110c3787297
diegovazpez <diego.vazpez@hotmail.com>
2022-02-01 12:03:29 -0600

    Se agrega databinding en ForgottenPassActivity

--- app/src/main/java/com/elektra/ektp/ektpforgottenpass/view/EKTPForgottenPassActivity.kt
+++ app/src/main/java/com/elektra/ektp/ektpforgottenpass/view/EKTPForgottenPassActivity.kt
@@ -2,11 +2,15 @@ package com.elektra.ektp.ektpforgottenpass.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.ActivityEktpforgottenPassBinding
 
 class EKTPForgottenPassActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEktpforgottenPassBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ektpforgotten_pass)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_ektpforgotten_pass)
     }
 }
