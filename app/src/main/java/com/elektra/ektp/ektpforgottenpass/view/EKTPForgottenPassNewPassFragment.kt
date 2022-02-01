ab3ece6cc3270b8b2dbe0cb2872e5280e235e3dd
diegovazpez <diego.vazpez@hotmail.com>
2022-02-01 12:30:14 -0600

    Se agregan variables globales y se inicializan botones

--- app/src/main/java/com/elektra/ektp/ektpforgottenpass/view/EKTPForgottenPassNewPassFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpforgottenpass/view/EKTPForgottenPassNewPassFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
 import com.elektra.ektp.databinding.FragmentEktpForgottenPassNewPassBinding
+import com.elektra.ektp.uservalidations.UserValidations
 
 class EKTPForgottenPassNewPassFragment : Fragment() {
 
     private lateinit var binding: FragmentEktpForgottenPassNewPassBinding
+    private val validations = UserValidations()
+    private var showPassVar = false
+    private var showPassVar2 = false
+    private var passTextVar = ""
+    private var passTextVar2 = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +27,9 @@ class EKTPForgottenPassNewPassFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_ektp_forgotten_pass_new_pass, container, false)
+
+        binding.buttonNewPass.isEnabled = false
+        binding.matchPass.isVisible = false
 
         return binding.root
     }
