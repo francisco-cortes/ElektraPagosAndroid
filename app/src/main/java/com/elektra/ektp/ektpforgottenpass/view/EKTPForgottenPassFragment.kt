6b131f9e72dadc41d824f31f1ac118a49ca6d55c
diegovazpez <diego.vazpez@hotmail.com>
2022-02-01 12:16:38 -0600

    Se agrega databinding en FOrgottenPassFragment

--- app/src/main/java/com/elektra/ektp/ektpforgottenpass/view/EKTPForgottenPassFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpforgottenpass/view/EKTPForgottenPassFragment.kt
@@ -5,16 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.FragmentEktpForgottenPassBinding
 
 class EKTPForgottenPassFragment : Fragment() {
+
+    private lateinit var binding: FragmentEktpForgottenPassBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ektp_forgotten_pass, container, false)
+        binding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_ektp_forgotten_pass, container, false)
+
+        return binding.root
     }
 
 }
