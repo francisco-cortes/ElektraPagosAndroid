21b4f40838e79c5d4bc96808aaaa48fe3c1f57b3
diegovazpez <diego.vazpez@hotmail.com>
2022-02-01 13:06:55 -0600

    Se agrega ForgottenPassAuthorizationCodeFragment vacío

---
+++ app/src/main/java/com/elektra/ektp/ektpforgottenpass/view/EKTPForgottenPassAuthorizationCodeFragment.kt
@@ -0,0 +1,23 @@
+package com.elektra.ektp.ektpforgottenpass.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.elektra.ektp.R
+
+class EKTPForgottenPassAuthorizationCodeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(
+            R.layout.fragment_ektp_forgotten_pass_authorization_code,
+            container,
+            false
+        )
+    }
+}
