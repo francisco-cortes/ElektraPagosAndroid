0876095e9b816102b2afd356c4db02e7986a9437
diegovazpez <diego.vazpez@hotmail.com>
2022-02-01 13:09:22 -0600

    Se agrega databinding en ForgottenPassAuthorizationCode

--- app/src/main/java/com/elektra/ektp/ektpforgottenpass/view/EKTPForgottenPassAuthorizationCodeFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpforgottenpass/view/EKTPForgottenPassAuthorizationCodeFragment.kt
@@ -5,19 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.FragmentEktpForgottenPassAuthorizationCodeBinding
+import com.elektra.ektp.databinding.FragmentEktpForgottenPassPhoneNumberBinding
 
 class EKTPForgottenPassAuthorizationCodeFragment : Fragment() {
+
+    private lateinit var binding: FragmentEktpForgottenPassAuthorizationCodeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(
-            R.layout.fragment_ektp_forgotten_pass_authorization_code,
-            container,
-            false
-        )
+        binding = DataBindingUtil.inflate(inflater,
+        R.layout.fragment_ektp_forgotten_pass_authorization_code, container, false)
+
+        return binding.root
     }
 }
