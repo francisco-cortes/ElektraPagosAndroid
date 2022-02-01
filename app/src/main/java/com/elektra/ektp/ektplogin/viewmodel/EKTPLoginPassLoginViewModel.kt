47ddb0cffc788ba4a7c959c8e2c487e7fedf4da7
Francisco Javier Cortes Torres <fcortest@EKT1779608FRANCISCOMACLAP.local>
2022-02-01 16:49:48 -0600

    view model de login pass creado

---
+++ app/src/main/java/com/elektra/ektp/ektplogin/viewmodel/EKTPLoginPassLoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.elektra.ektp.ektplogin.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.elektra.ektp.ektpsharedpreferences.EKPTUserApplication
+
+class EKTPLoginPassLoginViewModel : ViewModel() {
+}
