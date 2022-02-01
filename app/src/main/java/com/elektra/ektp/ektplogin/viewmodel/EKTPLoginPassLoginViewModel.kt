9345538fb93fc2770e283ea1a705cd1a4e291718
Francisco Javier Cortes Torres <fcortest@EKT1779608FRANCISCOMACLAP.local>
2022-02-01 16:53:16 -0600

    funcion get from shadpreferences a trvez de viewmodel

--- app/src/main/java/com/elektra/ektp/ektplogin/viewmodel/EKTPLoginPassLoginViewModel.kt
+++ app/src/main/java/com/elektra/ektp/ektplogin/viewmodel/EKTPLoginPassLoginViewModel.kt
@@ -4,4 +4,14 @@ import androidx.lifecycle.ViewModel
 import com.elektra.ektp.ektpsharedpreferences.EKPTUserApplication
 
 class EKTPLoginPassLoginViewModel : ViewModel() {
+
+    fun getSavedDataLogin() : ArrayList<String>{
+        val loginData = arrayListOf<String>()
+
+        loginData.add(EKPTUserApplication.preferences.getBioStatus().toString())
+        loginData.add(EKPTUserApplication.preferences.getBioType().toString())
+        loginData.add(EKPTUserApplication.preferences.getEncryptToken())
+
+        return  loginData
+    }
 }
