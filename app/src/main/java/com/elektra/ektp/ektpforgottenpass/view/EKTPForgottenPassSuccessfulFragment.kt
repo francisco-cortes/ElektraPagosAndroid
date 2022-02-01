28c15b5194333918397cf1cdf8c7d1180346f39c
diegovazpez <diego.vazpez@hotmail.com>
2022-02-01 17:22:07 -0600

    Se agrega databinding en SuccessfulFragment

--- app/src/main/java/com/elektra/ektp/ektpforgottenpass/view/EKTPForgottenPassSuccessfulFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpforgottenpass/view/EKTPForgottenPassSuccessfulFragment.kt
@@ -5,16 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.FragmentEktpForgottenPassSuccessfulBinding
 
 class EKTPForgottenPassSuccessfulFragment : Fragment() {
+
+    private lateinit var binding: FragmentEktpForgottenPassSuccessfulBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ektp_forgotten_pass_successful, container, false)
+        binding = DataBindingUtil.inflate(inflater,
+        R.layout.fragment_ektp_forgotten_pass_successful, container, false)
+
+        return binding.root
     }
 
 }
