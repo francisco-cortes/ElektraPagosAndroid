1aa67b2362280b6025bf145887040eb1b5b02730
Francisco Javier Cortes Torres <fcortest@EKT1779608FRANCISCOMACLAP.local>
2022-02-02 13:38:59 -0600

    ajusten en  el viewmodel

--- app/src/main/java/com/elektra/ektp/ektplogin/viewmodel/EKTPLoginActivityViewModel.kt
+++ app/src/main/java/com/elektra/ektp/ektplogin/viewmodel/EKTPLoginActivityViewModel.kt
@@ -2,20 +2,13 @@ package com.elektra.ektp.ektplogin.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.elektra.ektp.ektpsharedpreferences.EKTPUserApplication
+import com.elektra.ektp.ektpsharedpreferences.EKTPUserApplication.Companion.preferences
 
 class EKTPLoginActivityViewModel : ViewModel() {
 
     fun saveBiometricStatus(bioStatus: Int, bioType: Int){
-        EKTPUserApplication.preferences.saveBioStatus(bioStatus)
-        EKTPUserApplication.preferences.saveBioType(bioType)
+        preferences.saveBioStatus(bioStatus)
+        preferences.saveBioType(bioType)
     }
 
-    fun userIsEnrolled () : Boolean {
-        val isEnrolled = false
-        val name = EKTPUserApplication.preferences.getNameUser()
-
-        if (name != null || name != "") isEnrolled == false
-
-        return isEnrolled
-    }
 }
