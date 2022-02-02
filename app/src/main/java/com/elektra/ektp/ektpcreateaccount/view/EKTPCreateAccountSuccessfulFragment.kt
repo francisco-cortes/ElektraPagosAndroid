f8c29ad415fe5ec0a120650617467590f0f0e0bb
diegovazpez <diego.vazpez@hotmail.com>
2022-02-02 17:09:28 -0600

    Se agrega función onCreate con handler para terminar proceso de activity

--- app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKTPCreateAccountSuccessfulFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpcreateaccount/view/EKTPCreateAccountSuccessfulFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
 import com.elektra.ektp.databinding.FragmentEktpCreateAccountSuccessfulBinding
@@ -12,6 +13,16 @@ import com.elektra.ektp.databinding.FragmentEktpCreateAccountSuccessfulBinding
 class EKTPCreateAccountSuccessfulFragment : Fragment() {
 
     private lateinit var binding: FragmentEktpCreateAccountSuccessfulBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.onBackPressedDispatcher?.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                isEnabled = false
+                activity?.onBackPressed()
+            }
+        })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
