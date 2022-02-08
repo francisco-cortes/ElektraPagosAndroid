7b822d3220374e4b83eb7a436a2a73f3ca290a86
diegovazpez <diego.vazpez@hotmail.com>
2022-02-08 12:08:42 -0600

    Se agrega activity Movements vacía

---
+++ app/src/main/java/com/elektra/ektp/ektpmovements/view/EKTPMovementsActivity.kt
@@ -0,0 +1,12 @@
+package com.elektra.ektp.ektpmovements.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elektra.ektp.R
+
+class EKTPMovementsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ektp_movements)
+    }
+}
