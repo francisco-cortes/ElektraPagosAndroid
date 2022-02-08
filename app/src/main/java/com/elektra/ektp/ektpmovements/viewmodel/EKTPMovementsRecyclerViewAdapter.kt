cfb2cc21eb4defce8452d5877020589db8bc6b22
diegovazpez <diego.vazpez@hotmail.com>
2022-02-08 13:47:41 -0600

    Se agregan funciones miembro en RecyclerViewAdapter para MovementsRecyclerView vacío

--- app/src/main/java/com/elektra/ektp/ektpmovements/viewmodel/EKTPMovementsRecyclerViewAdapter.kt
+++ app/src/main/java/com/elektra/ektp/ektpmovements/viewmodel/EKTPMovementsRecyclerViewAdapter.kt
@@ -1,4 +1,32 @@
 package com.elektra.ektp.ektpmovements.viewmodel
 
-class EKTPMovementsRecyclerViewAdapter {
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.elektra.ektp.databinding.ItemMovementBinding
+import com.elektra.ektp.ektpmovements.model.EKTPMovementsModel
+
+class EKTPMovementsRecyclerViewAdapter(
+    private val context: Context,
+    private val data: List<EKTPMovementsModel>
+) : RecyclerView.Adapter<EKTPMovementsRecyclerViewAdapter.MovementsViewHolder>() {
+
+    inner class MovementsViewHolder(val binding: ItemMovementBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(item: EKTPMovementsModel){
+            binding.movementsListItem = item
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovementsViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: MovementsViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        
+    }
+
 }
