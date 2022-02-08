c66307beefc9f1be15f9205e1641b3058917ccff
diegovazpez <diego.vazpez@hotmail.com>
2022-02-08 13:52:15 -0600

    Se agregan datos hardcode para modelo en Movements

--- app/src/main/java/com/elektra/ektp/ektpmovements/view/EKTPMovementsListFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpmovements/view/EKTPMovementsListFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
 import com.elektra.ektp.databinding.FragmentEktpMovementsListBinding
+import com.elektra.ektp.ektpmovements.model.EKTPMovementsModel
 
 class EKTPMovementsListFragment : Fragment() {
     
@@ -25,6 +26,54 @@ private lateinit var binding: FragmentEktpMovementsListBinding
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, 
             R.layout.fragment_ektp_movements_list, container, false)
+
+        val data = listOf(
+            EKTPMovementsModel("+ $500.00","Orden de Pago", "23/12/2021","Cuenta destino ****1234",
+                "Francisco Javier Cortes Torres", "Envío de dinero", "12345678",
+                "Transferencia exitosa"),
+            EKTPMovementsModel("- $500.00","Orden de Pago", "24/12/2021","Cuenta destino ****1234",
+                "Francisco Javier Cortes Torres", "Envío de dinero", "12345678",
+                "Transferencia exitosa"),
+            EKTPMovementsModel("+ $500.00","Orden de Pago", "25/12/2021","Cuenta destino ****1234",
+                "Francisco Javier Cortes Torres", "Envío de dinero", "12345678",
+                "Transferencia exitosa"),
+            EKTPMovementsModel("- $500.00","Orden de Pago", "26/12/2021","Cuenta destino ****1234",
+                "Francisco Javier Cortes Torres", "Envío de dinero", "12345678",
+                "Transferencia exitosa"),
+            EKTPMovementsModel("- $500.00","Orden de Pago", "27/12/2021","Cuenta destino ****1234",
+                "Francisco Javier Cortes Torres", "Envío de dinero", "12345678",
+                "Transferencia exitosa"),
+            EKTPMovementsModel("+ $500.00","Orden de Pago", "28/12/2021","Cuenta destino ****1234",
+                "Francisco Javier Cortes Torres", "Envío de dinero", "12345678",
+                "Transferencia exitosa"),
+            EKTPMovementsModel("- $500.00","Orden de Pago", "29/12/2021","Cuenta destino ****1234",
+                "Francisco Javier Cortes Torres", "Envío de dinero", "12345678",
+                "Transferencia exitosa"),
+            EKTPMovementsModel("+ $500.00","Orden de Pago", "30/12/2021","Cuenta destino ****1234",
+                "Francisco Javier Cortes Torres", "Envío de dinero", "12345678",
+                "Transferencia exitosa"),
+            EKTPMovementsModel("- $500.00","Orden de Pago", "31/12/2021","Cuenta destino ****1234",
+                "Francisco Javier Cortes Torres", "Envío de dinero", "12345678",
+                "Transferencia exitosa"),
+            EKTPMovementsModel("- $500.00","Orden de Pago", "01/01/2022","Cuenta destino ****1234",
+                "Francisco Javier Cortes Torres", "Envío de dinero", "12345678",
+                "Transferencia exitosa"),
+            EKTPMovementsModel("+ $500.00","Orden de Pago", "02/01/2022","Cuenta destino ****1234",
+                "Francisco Javier Cortes Torres", "Envío de dinero", "12345678",
+                "Transferencia exitosa"),
+            EKTPMovementsModel("- $500.00","Orden de Pago", "03/01/2022","Cuenta destino ****1234",
+                "Francisco Javier Cortes Torres", "Envío de dinero", "12345678",
+                "Transferencia exitosa"),
+            EKTPMovementsModel("+ $500.00","Orden de Pago", "04/01/2022","Cuenta destino ****1234",
+                "Francisco Javier Cortes Torres", "Envío de dinero", "12345678",
+                "Transferencia exitosa"),
+            EKTPMovementsModel("- $500.00","Orden de Pago", "05/01/2022","Cuenta destino ****1234",
+                "Francisco Javier Cortes Torres", "Envío de dinero", "12345678",
+                "Transferencia exitosa"),
+            EKTPMovementsModel("- $500.00","Orden de Pago", "06/01/2022","Cuenta destino ****1234",
+                "Francisco Javier Cortes Torres", "Envío de dinero", "12345678",
+                "Transferencia exitosa")
+        )
         
         return binding.root
     }
