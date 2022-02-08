aeae13366afcfbb34b82ee9dc2cd433c57cf1c77
diegovazpez <diego.vazpez@hotmail.com>
2022-02-08 15:36:30 -0600

    Se configura RecyclerView para MovementsList

--- app/src/main/java/com/elektra/ektp/ektpmovements/viewmodel/EKTPMovementsRecyclerViewAdapter.kt
+++ app/src/main/java/com/elektra/ektp/ektpmovements/viewmodel/EKTPMovementsRecyclerViewAdapter.kt
@@ -1,10 +1,14 @@
 package com.elektra.ektp.ektpmovements.viewmodel
 
 import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.elektra.ektp.databinding.ItemMovementBinding
 import com.elektra.ektp.ektpmovements.model.EKTPMovementsModel
+import com.elektra.ektp.ektpmovements.view.EKTPMovementsDetailsActivity
 
 class EKTPMovementsRecyclerViewAdapter(
     private val context: Context,
@@ -18,15 +22,31 @@ class EKTPMovementsRecyclerViewAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovementsViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
 
+        val listItemBinding = ItemMovementBinding.inflate(inflater, parent, false)
+
+        return MovementsViewHolder(listItemBinding)
     }
 
     override fun onBindViewHolder(holder: MovementsViewHolder, position: Int) {
-
+        holder.bind(data[position])
+        holder.binding.root.setOnClickListener {
+            var moveIntent = Intent(context, EKTPMovementsDetailsActivity::class.java)
+            val msg = data[position]
+            moveIntent.putExtra("detailDate", msg.detailDate)
+            moveIntent.putExtra("detailTitle", msg.detailTitle)
+            moveIntent.putExtra("detailConcept", msg.detailConcept)
+            moveIntent.putExtra("detailAmount", msg.detailAmount)
+            moveIntent.putExtra("detailStatus", msg.detailStatus)
+            moveIntent.putExtra("detailName", msg.detailName)
+            moveIntent.putExtra("detailFolio", msg.detailFolio)
+            moveIntent.putExtra("detailAccount", msg.detailAccount)
+            context.startActivity(moveIntent)
+            Toast.makeText(holder.binding.root.context, "Elemento ${position + 1} seleccionado", Toast.LENGTH_SHORT).show()
+        }
     }
 
-    override fun getItemCount(): Int {
-        
-    }
+    override fun getItemCount(): Int = data.size
 
 }
