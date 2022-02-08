21d4ba45fb89a9debac9393ce696d28d19ced98e
diegovazpez <diego.vazpez@hotmail.com>
2022-02-08 15:46:46 -0600

    Se agregan funciones para crear imagen a partir de layout inflated y compartir bitmap creado

--- app/src/main/java/com/elektra/ektp/ektpmovements/view/EKTPMovementsDetailsActivity.kt
+++ app/src/main/java/com/elektra/ektp/ektpmovements/view/EKTPMovementsDetailsActivity.kt
@@ -1,11 +1,21 @@
 package com.elektra.ektp.ektpmovements.view
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
 import com.elektra.ektp.databinding.ActivityEktpMovementsDetailsBinding
 import com.elektra.ektp.ektpmovements.model.EKTPMovementsModel
+import java.io.File
+import java.io.FileOutputStream
 
 class EKTPMovementsDetailsActivity : AppCompatActivity() {
 
@@ -37,5 +47,42 @@ class EKTPMovementsDetailsActivity : AppCompatActivity() {
 
         binding.movementsDetailsItem = dataList
         
+    }
+
+    @SuppressLint("SetWorldReadable")
+    private fun shareImage(bitMap: Bitmap?) {
+        try {
+            val mediaImage = File(this.externalCacheDir, "movementDetails.png")
+            val fOut = FileOutputStream(mediaImage)
+            bitMap?.compress(Bitmap.CompressFormat.PNG, 100, fOut)
+            fOut.flush()
+            fOut.close()
+            mediaImage.setReadable(true, false)
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            val uriIntent = FileProvider.getUriForFile(this, "com.inteligenciadedatoseinovacion.elektrapagos", mediaImage)
+            intent.putExtra(Intent.EXTRA_STREAM, uriIntent)
+            intent.type = "image/png"
+            startActivity(Intent.createChooser(intent, "Compartir imagen vía"))
+        }
+        catch (e: Exception){
+            Log.e("FGF", "Error al compartir imagen: " + e.message)
+        }
+    }
+
+    private fun getScreenShot(view: View): Bitmap?{
+        var screenshot: Bitmap? = null
+        try {
+            val totalH = binding.detailsCardViewToShare.getChildAt(0).height
+            screenshot = Bitmap.createBitmap(view.measuredWidth, totalH, Bitmap.Config.ARGB_8888)
+            // Now draw this bitmap on a canvas
+            val canvas = Canvas(screenshot)
+            canvas.drawColor(Color.WHITE)
+            view.draw(canvas)
+        } catch (e: Exception) {
+            Log.e("GFG", "Error al generar imagen: " + e.message)
+        }
+        // return the bitmap
+        return screenshot
     }
 }
