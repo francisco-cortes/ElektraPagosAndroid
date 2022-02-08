d409282d72510bd157588ae4d0ebef8c1c84899c
Francisco Javier Cortes Torres <fcortest@EKT1779608FRANCISCOMACLAP.local>
2022-02-08 15:13:36 -0600

    activity para el flujo de recibir dinero creado, y portrair orientation agregado al manifest

---
+++ app/src/main/java/com/elektra/ektp/ektpreceivemoney/view/EKTPReceiveMoneyActivity.kt
@@ -0,0 +1,12 @@
+package com.elektra.ektp.ektpreceivemoney.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elektra.ektp.R
+
+class EKTPReceiveMoneyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ektpreceive_money)
+    }
+}
