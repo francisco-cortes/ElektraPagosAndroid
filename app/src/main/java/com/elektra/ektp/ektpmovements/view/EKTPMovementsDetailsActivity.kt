61a5b08fe8390bf08e0a24dcb1e21dafb1761fcd
diegovazpez <diego.vazpez@hotmail.com>
2022-02-08 13:02:35 -0600

    Se agrega databinding en MovementsDetailsActivity

--- app/src/main/java/com/elektra/ektp/ektpmovements/view/EKTPMovementsDetailsActivity.kt
+++ app/src/main/java/com/elektra/ektp/ektpmovements/view/EKTPMovementsDetailsActivity.kt
@@ -2,11 +2,16 @@ package com.elektra.ektp.ektpmovements.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
+import com.elektra.ektp.databinding.ActivityEktpMovementsDetailsBinding
 
 class EKTPMovementsDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEktpMovementsDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ektp_movements_details)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_ektp_movements_details)
     }
 }
