215fa0e3c41e5b40fcaf8f13891a07a7b60105ad
Francisco Javier Cortes Torres <fcortest@EKT1779608FRANCISCOMACLAP.local>
2022-02-08 20:05:17 -0600

    fragment mtcn recibido

---
+++ app/src/main/java/com/elektra/ektp/ektpreceivemoney/view/ReceiveMTCNAcountSuccesFragment.kt
@@ -0,0 +1,24 @@
+package com.elektra.ektp.ektpreceivemoney.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.elektra.ektp.R
+import com.elektra.ektp.databinding.FragmentReceiveMtcnAcountSuccesBinding
+
+class ReceiveMTCNAcountSuccesFragment : Fragment() {
+
+    private lateinit var binding: FragmentReceiveMtcnAcountSuccesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding =  DataBindingUtil.inflate<FragmentReceiveMtcnAcountSuccesBinding>(inflater,R.layout.fragment_receive_mtcn_acount_succes, container, false)
+        return binding.root
+    }
+}
