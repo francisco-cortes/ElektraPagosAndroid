1565b6adf7398a9d9014bdbd05bded204200a45a
diegovazpez <diego.vazpez@hotmail.com>
2022-02-11 11:46:14 -0600

    Se agregan comentarios en NewPassword y se reducen las invocaciones de databinding con WITH Instance

--- app/src/main/java/com/elektra/ektp/ektpforgottenpass/view/EKTPForgottenPassNewPassFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpforgottenpass/view/EKTPForgottenPassNewPassFragment.kt
@@ -21,16 +21,19 @@ import com.elektra.ektp.uservalidations.UserValidations
 
 class EKTPForgottenPassNewPassFragment : Fragment(){
 
+    //Global databinding access variable
     private lateinit var binding: FragmentEktpForgottenPassNewPassBinding
+    //UserValidations access variable
     private val validations = UserValidations()
+    //General data variables
     private var showPassVar = false
     private var showPassVar2 = false
     private var passTextVar = ""
     private var passTextVar2 = ""
-    val flagBackStack = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Overriding OnBackPressed function to destroy fragment and activity
         activity?.onBackPressedDispatcher?.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                     findNavController().popBackStack()
@@ -46,184 +49,190 @@ class EKTPForgottenPassNewPassFragment : Fragment(){
         binding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_ektp_forgotten_pass_new_pass, container, false)
 
-        binding.buttonNewPass.isEnabled = false
-        binding.matchPass.isVisible = false
+        //Wrap this block code for all the lines with binding variable
+        with(binding){
+            buttonNewPass.isEnabled = false
+            matchPass.isVisible = false
 
-        binding.insertNewPass.addTextChangedListener(object: TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            //TextWatcher function to listen for changes on editText
+            insertNewPass.addTextChangedListener(object: TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
-            }
+                }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                passTextVar = s.toString()
-                if(passTextVar.length < 8 ){
-                    binding.notMinIcon.isVisible = true
-                    binding.okMinIcon.isVisible = false
-                    binding.buttonNewPass.isEnabled = false
-                }
-                else{
-                    binding.okMinIcon.isVisible = true
-                    binding.notMinIcon.isVisible = false
-                }
-                if (validations.checkRepeatedChars(passTextVar)){
-                    binding.okRepeatedIcon.isVisible = true
-                    binding.notRepeatedIcon.isVisible = false
-                }
-                else{
-                    binding.okRepeatedIcon.isVisible = false
-                    binding.notRepeatedIcon.isVisible = true
-                    binding.buttonNewPass.isEnabled = false
-                }
-                if (validations.checkBankString(passTextVar)){
-                    binding.okNameIcon.isVisible = true
-                    binding.notNameIcon.isVisible = false
-                }
-                else{
-                    binding.okNameIcon.isVisible = false
-                    binding.notNameIcon.isVisible = true
-                    binding.buttonNewPass.isEnabled = false
-                }
-                if (validations.checkConsecutiveString(passTextVar)){
-                    binding.okMConsecutiveIcon.isVisible = true
-                    binding.notConsecutiveIcon.isVisible = false
-                }
-                else{
-                    binding.okMConsecutiveIcon.isVisible = false
-                    binding.notConsecutiveIcon.isVisible = true
-                    binding.buttonNewPass.isEnabled = false
-                }
-                if(passTextVar.length in 8..14 ){
-                    binding.okMaxIcon.isVisible = true
-                    binding.notMaxIcon.isVisible = false
-                }
-                else{
-                    binding.okMaxIcon.isVisible = false
-                    binding.notMaxIcon.isVisible = true
-                    binding.buttonNewPass.isEnabled = false
-                }
-                if (passTextVar.length in 8..14 && validations.checkRepeatedChars(passTextVar) && validations.checkBankString(passTextVar)
-                    && validations.checkConsecutiveString(passTextVar)){
-                    binding.insertNewPass.setBackgroundResource(R.drawable.rounded_rectangle_gray)
-                }
-                else{
-                    binding.insertNewPass.setBackgroundResource(R.drawable.validation_edit_text)
-                    binding.buttonNewPass.isEnabled = false
-                }
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                passTextVar = s.toString()
-                if (passTextVar.length in 8..14){
-                    if (passTextVar != passTextVar2){
-                        binding.matchPass.isVisible = true
-                        binding.notMatchesIcon.isVisible = true
-                        binding.buttonNewPass.isEnabled = false
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    passTextVar = s.toString()
+                    if (validations.checkRepeatedChars(passTextVar)){
+                        okRepeatedIcon.isVisible = true
+                        notRepeatedIcon.isVisible = false
                     }
                     else{
-                        binding.notMatchesIcon.isVisible = false
-                        binding.matchPass.isVisible = false
-                        binding.buttonNewPass.isEnabled = (passTextVar.length in 8..14 && validations.checkRepeatedChars(passTextVar) && validations.checkBankString(passTextVar)
-                                && validations.checkConsecutiveString(passTextVar))
+                        okRepeatedIcon.isVisible = false
+                        notRepeatedIcon.isVisible = true
+                        buttonNewPass.isEnabled = false
                     }
-                }
-            }
-        })
-
-        binding.insertConfirmPass.addTextChangedListener(object: TextWatcher{
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                passTextVar2 = s.toString()
-                if (passTextVar.length in 8..14){
-                    if (passTextVar != passTextVar2){
-                        binding.insertConfirmPass.setBackgroundResource(R.drawable.validation_edit_text)
-                        binding.notMatchesIcon.isVisible = true
-                        binding.matchPass.isVisible = true
-                        binding.buttonNewPass.isEnabled = false
+                    if (validations.checkBankString(passTextVar)){
+                        okNameIcon.isVisible = true
+                        notNameIcon.isVisible = false
                     }
                     else{
-                        binding.insertConfirmPass.setBackgroundResource(R.drawable.rounded_rectangle_gray)
-                        binding.notMatchesIcon.isVisible = false
-                        binding.matchPass.isVisible = false
-                        binding.buttonNewPass.isEnabled = (passTextVar.length in 8..14 && validations.checkRepeatedChars(passTextVar) && validations.checkBankString(passTextVar)
-                                && validations.checkConsecutiveString(passTextVar))
+                        okNameIcon.isVisible = false
+                        notNameIcon.isVisible = true
+                        buttonNewPass.isEnabled = false
                     }
-                }
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                passTextVar2 = s.toString()
-                if (passTextVar.length in 8..14){
-                    if (passTextVar != passTextVar2){
-                        binding.insertConfirmPass.setBackgroundResource(R.drawable.validation_edit_text)
-                        binding.notMatchesIcon.isVisible = true
-                        binding.matchPass.isVisible = true
-                        binding.buttonNewPass.isEnabled = false
+                    if (validations.checkConsecutiveString(passTextVar)){
+                        okMConsecutiveIcon.isVisible = true
+                        notConsecutiveIcon.isVisible = false
                     }
                     else{
-                        binding.insertConfirmPass.setBackgroundResource(R.drawable.rounded_rectangle_gray)
-                        binding.notMatchesIcon.isVisible = false
-                        binding.matchPass.isVisible = false
-                        binding.buttonNewPass.isEnabled = (passTextVar.length in 8..14 && validations.checkRepeatedChars(passTextVar) && validations.checkBankString(passTextVar)
-                                && validations.checkConsecutiveString(passTextVar))
+                        okMConsecutiveIcon.isVisible = false
+                        notConsecutiveIcon.isVisible = true
+                        buttonNewPass.isEnabled = false
                     }
-                }
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                passTextVar2 = s.toString()
-                if (passTextVar.length in 8..14){
-                    if (passTextVar != passTextVar2){
-                        binding.insertConfirmPass.setBackgroundResource(R.drawable.validation_edit_text)
-                        binding.notMatchesIcon.isVisible = true
-                        binding.matchPass.isVisible = true
-                        binding.buttonNewPass.isEnabled = false
+                    if(passTextVar.length in 8..14 ){
+                        okMaxIcon.isVisible = true
+                        notMaxIcon.isVisible = false
                     }
                     else{
-                        binding.insertConfirmPass.setBackgroundResource(R.drawable.rounded_rectangle_gray)
-                        binding.notMatchesIcon.isVisible = false
-                        binding.matchPass.isVisible = false
-                        binding.buttonNewPass.isEnabled = (passTextVar.length in 8..14 && validations.checkRepeatedChars(passTextVar) && validations.checkBankString(passTextVar)
-                                && validations.checkConsecutiveString(passTextVar))
+                        okMaxIcon.isVisible = false
+                        notMaxIcon.isVisible = true
+                        buttonNewPass.isEnabled = false
+                    }
+                    if (passTextVar.length in 8..14 && validations.checkRepeatedChars(passTextVar) && validations.checkBankString(passTextVar)
+                        && validations.checkConsecutiveString(passTextVar)){
+                        insertNewPass.setBackgroundResource(R.drawable.rounded_rectangle_gray)
+                    }
+                    else{
+                        insertNewPass.setBackgroundResource(R.drawable.validation_edit_text)
+                        buttonNewPass.isEnabled = false
                     }
                 }
-            }
-        })
 
-        binding.showPassButton1.setOnClickListener {
+                override fun afterTextChanged(s: Editable?) {
+                    passTextVar = s.toString()
+                    if (passTextVar.length in 8..14){
+                        if (passTextVar != passTextVar2){
+                            matchPass.isVisible = true
+                            notMatchesIcon.isVisible = true
+                            buttonNewPass.isEnabled = false
+                        }
+                        else{
+                            notMatchesIcon.isVisible = false
+                            matchPass.isVisible = false
+                            buttonNewPass.isEnabled = (passTextVar.length in 8..14 && validations.checkRepeatedChars(passTextVar) && validations.checkBankString(passTextVar)
+                                    && validations.checkConsecutiveString(passTextVar))
+                        }
+                    }
+                }
+            })
+            //---
 
-            if (!showPassVar) {
-                binding.insertNewPass.transformationMethod = HideReturnsTransformationMethod.getInstance()
-                binding.showPassButton1.setBackgroundResource(R.drawable.ic_hide_pass)
-                showPassVar = true
+            //TextWatcher function to listen for changes on editText
+            insertConfirmPass.addTextChangedListener(object: TextWatcher{
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                    passTextVar2 = s.toString()
+                    if (passTextVar.length in 8..14){
+                        if (passTextVar != passTextVar2){
+                            insertConfirmPass.setBackgroundResource(R.drawable.validation_edit_text)
+                            notMatchesIcon.isVisible = true
+                            matchPass.isVisible = true
+                            buttonNewPass.isEnabled = false
+                        }
+                        else{
+                            insertConfirmPass.setBackgroundResource(R.drawable.rounded_rectangle_gray)
+                            notMatchesIcon.isVisible = false
+                            matchPass.isVisible = false
+                            buttonNewPass.isEnabled = (passTextVar.length in 8..14 && validations.checkRepeatedChars(passTextVar) && validations.checkBankString(passTextVar)
+                                    && validations.checkConsecutiveString(passTextVar))
+                        }
+                    }
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    passTextVar2 = s.toString()
+                    if (passTextVar.length in 8..14){
+                        if (passTextVar != passTextVar2){
+                            insertConfirmPass.setBackgroundResource(R.drawable.validation_edit_text)
+                            notMatchesIcon.isVisible = true
+                            matchPass.isVisible = true
+                            buttonNewPass.isEnabled = false
+                        }
+                        else{
+                            insertConfirmPass.setBackgroundResource(R.drawable.rounded_rectangle_gray)
+                            notMatchesIcon.isVisible = false
+                            matchPass.isVisible = false
+                            buttonNewPass.isEnabled = (passTextVar.length in 8..14 && validations.checkRepeatedChars(passTextVar) && validations.checkBankString(passTextVar)
+                                    && validations.checkConsecutiveString(passTextVar))
+                        }
+                    }
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    passTextVar2 = s.toString()
+                    if (passTextVar.length in 8..14){
+                        if (passTextVar != passTextVar2){
+                            insertConfirmPass.setBackgroundResource(R.drawable.validation_edit_text)
+                            notMatchesIcon.isVisible = true
+                            matchPass.isVisible = true
+                            buttonNewPass.isEnabled = false
+                        }
+                        else{
+                            insertConfirmPass.setBackgroundResource(R.drawable.rounded_rectangle_gray)
+                            notMatchesIcon.isVisible = false
+                            matchPass.isVisible = false
+                            buttonNewPass.isEnabled = (passTextVar.length in 8..14 && validations.checkRepeatedChars(passTextVar) && validations.checkBankString(passTextVar)
+                                    && validations.checkConsecutiveString(passTextVar))
+                        }
+                    }
+                }
+            })
+            //---
+
+            //onClickListener on show/hidePass to listen for user interaction with password visibility
+            showPassButton1.setOnClickListener {
+
+                if (!showPassVar) {
+                    insertNewPass.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                    showPassButton1.setBackgroundResource(R.drawable.ic_hide_pass)
+                    showPassVar = true
+                }
+                else {
+                    insertNewPass.transformationMethod = PasswordTransformationMethod.getInstance()
+                    showPassButton1.setBackgroundResource(R.drawable.ic_show_pass)
+                    showPassVar = false
+                }
             }
-            else {
-                binding.insertNewPass.transformationMethod = PasswordTransformationMethod.getInstance()
-                binding.showPassButton1.setBackgroundResource(R.drawable.ic_show_pass)
-                showPassVar = false
+            //---
+
+            //onClickListener on show/hidePassConfirmation to listen for user interaction with password visibility
+            showPassButton2.setOnClickListener {
+
+                if (!showPassVar2) {
+                    insertConfirmPass.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                    showPassButton2.setBackgroundResource(R.drawable.ic_hide_pass)
+                    showPassVar2 = true
+                }
+                else {
+                    insertConfirmPass.transformationMethod = PasswordTransformationMethod.getInstance()
+                    showPassButton2.setBackgroundResource(R.drawable.ic_show_pass)
+                    showPassVar2 = false
+                }
             }
+            //---
+
+            //onClickListener on appBar BackButton to popBackStack fragment
+            backAppbarButton.setOnClickListener { view : View ->
+                view.findNavController().popBackStack()
+            }
+            //---
+
+            //onClickListener on continueButton to navigate to biometricsActivation or Successful createAccount according to sharedPreferences
+            buttonNewPass.setOnClickListener { view: View ->
+                view.findNavController().navigate(R.id.action_EKTPForgottenPassNewPassFragment_to_EKTPForgottenPassSuccessfulFragment)
+            }
+            //---
+
+            return root
         }
-
-        binding.showPassButton2.setOnClickListener {
-
-            if (!showPassVar2) {
-                binding.insertConfirmPass.transformationMethod = HideReturnsTransformationMethod.getInstance()
-                binding.showPassButton2.setBackgroundResource(R.drawable.ic_hide_pass)
-                showPassVar2 = true
-            }
-            else {
-                binding.insertConfirmPass.transformationMethod = PasswordTransformationMethod.getInstance()
-                binding.showPassButton2.setBackgroundResource(R.drawable.ic_show_pass)
-                showPassVar2 = false
-            }
-        }
-
-        binding.backAppbarButton.setOnClickListener { view : View ->
-            view.findNavController().popBackStack()
-        }
-
-        binding.buttonNewPass.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_EKTPForgottenPassNewPassFragment_to_EKTPForgottenPassSuccessfulFragment)
-        }
-
-        return binding.root
     }
 }
