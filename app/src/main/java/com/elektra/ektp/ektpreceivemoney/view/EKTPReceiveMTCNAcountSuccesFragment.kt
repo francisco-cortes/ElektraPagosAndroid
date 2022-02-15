4b517cee30d2fe12fda99a4349c332919d194c40
diegovazpez <diego.vazpez@hotmail.com>
2022-02-15 15:18:25 -0600

    Se agrega función crear y compartir imagen en receive money, se ajusta icono de show/hide password en loginPass, se ajusta padding en sharebuttonDetails en MovementsDetails

--- app/src/main/java/com/elektra/ektp/ektpreceivemoney/view/EKTPReceiveMTCNAcountSuccesFragment.kt
+++ app/src/main/java/com/elektra/ektp/ektpreceivemoney/view/EKTPReceiveMTCNAcountSuccesFragment.kt
@@ -1,13 +1,24 @@
 package com.elektra.ektp.ektpreceivemoney.view
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
 import com.elektra.ektp.R
 import com.elektra.ektp.databinding.FragmentReceiveMtcnAcountSuccesBinding
+import java.io.File
+import java.io.FileOutputStream
 
 class EKTPReceiveMTCNAcountSuccesFragment : Fragment() {
 
@@ -22,6 +33,53 @@ class EKTPReceiveMTCNAcountSuccesFragment : Fragment() {
         binding.doneButton.setOnClickListener {
             activity?.finish()
         }
+
+        binding.shareDetailsButton.setOnClickListener{
+            view: View ->
+            Toast.makeText(activity, "Compartiendo detalles de movimiento", Toast.LENGTH_SHORT)
+                .show()
+            val bitMap = getScreenShot(binding.detailsCardViewToShare)
+            shareImage(bitMap)
+
+        }
+
         return binding.root
+    }
+
+    @SuppressLint("SetWorldReadable")
+    private fun shareImage(bitMap: Bitmap?) {
+        try {
+            val mediaImage = File(activity?.externalCacheDir, "movementDetails.png")
+            val fOut = FileOutputStream(mediaImage)
+            bitMap?.compress(Bitmap.CompressFormat.PNG, 100, fOut)
+            fOut.flush()
+            fOut.close()
+            mediaImage.setReadable(true, false)
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            val uriIntent = FileProvider.getUriForFile(activity as Context, "com.elektra.ektp", mediaImage)
+            intent.putExtra(Intent.EXTRA_STREAM, uriIntent)
+            intent.type = "image/png"
+            startActivity(Intent.createChooser(intent, "Compartir imagen vía"))
+        }
+        catch (e: Exception){
+            Log.e("FGF", "Error al compartir imagen: " + e.message)
+        }
+    }
+
+    private fun getScreenShot(view: View): Bitmap?{
+        var screenshot: Bitmap? = null
+        try {
+            val totalH = binding.detailsCardViewToShare.getChildAt(0).height
+            screenshot = Bitmap.createBitmap(view.measuredWidth, totalH, Bitmap.Config.ARGB_8888)
+            // Now draw this bitmap on a canvas
+            val canvas = Canvas(screenshot)
+            canvas.drawColor(Color.WHITE)
+            view.draw(canvas)
+        } catch (e: Exception) {
+            Log.e("GFG", "Error al generar imagen: " + e.message)
+        }
+        // return the bitmap
+        return screenshot
     }
 }
