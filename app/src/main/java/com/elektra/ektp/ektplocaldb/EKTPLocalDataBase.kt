5988dcebc974de83070cd92b8e07bb8ac5fa79af
francisco <fj23pk@hotmail.com>
2022-03-25 20:21:06 -0600

    abstract class for database

---
+++ app/src/main/java/com/elektra/ektp/ektplocaldb/EKTPLocalDataBase.kt
@@ -0,0 +1,12 @@
+package com.elektra.ektp.ektplocaldb
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.elektra.ektp.ektplocaldb.dao.EKTPLocalUserDAO
+import com.elektra.ektp.ektplocaldb.localentity.EKTPLocalUser
+
+@Database(entities = [EKTPLocalUser::class], version = 1, )
+abstract class EKTPLocalDataBase : RoomDatabase() {
+
+    abstract fun localUserDAO(): EKTPLocalUserDAO
+}
