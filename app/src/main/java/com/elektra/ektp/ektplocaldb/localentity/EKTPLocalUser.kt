2afdc09ed82633bea3fee1a76e7af10eccd35080
francisco <fj23pk@hotmail.com>
2022-03-25 19:16:10 -0600

    nonnull label on local user dataclass

--- app/src/main/java/com/elektra/ektp/ektplocaldb/localentity/EKTPLocalUser.kt
+++ app/src/main/java/com/elektra/ektp/ektplocaldb/localentity/EKTPLocalUser.kt
@@ -1,5 +1,6 @@
 package com.elektra.ektp.ektplocaldb.localentity
 
+import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -7,22 +8,22 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "LocalUser")
 data class EKTPLocalUser(
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id") val id: Int = 0,
-    @ColumnInfo(name = "userName") val userName: String,
-    @ColumnInfo(name = "userPaternalName") val userPaternalName: String,
+    @NonNull @ColumnInfo(name = "id") val id: Int = 0,
+    @NonNull @ColumnInfo(name = "userName") val userName: String,
+    @NonNull @ColumnInfo(name = "userPaternalName") val userPaternalName: String,
     @ColumnInfo(name = "userMaternalName") val userMaternalName: String,
-    @ColumnInfo(name = "userBirthDate") val userBirthDate: String,
-    @ColumnInfo(name = "userBirthPlace") val userBirthPlace: String,
-    @ColumnInfo(name = "userGenre") val userGenre: String,
-    @ColumnInfo(name = "userTelNum") val userTelNum: String,
-    @ColumnInfo(name = "userEmail") val userEmail: String,
-    @ColumnInfo(name = "userPC") val userPC: String,
-    @ColumnInfo(name = "userSettlement") val userSettlement: String,
-    @ColumnInfo(name = "userStreet") val userStreet: String,
-    @ColumnInfo(name = "userExtNum") val userExtNum: String,
+    @NonNull @ColumnInfo(name = "userBirthDate") val userBirthDate: String,
+    @NonNull @ColumnInfo(name = "userBirthPlace") val userBirthPlace: String,
+    @NonNull @ColumnInfo(name = "userGenre") val userGenre: String,
+    @NonNull @ColumnInfo(name = "userTelNum") val userTelNum: String,
+    @NonNull @ColumnInfo(name = "userEmail") val userEmail: String,
+    @NonNull @ColumnInfo(name = "userPC") val userPC: String,
+    @NonNull @ColumnInfo(name = "userSettlement") val userSettlement: String,
+    @NonNull @ColumnInfo(name = "userStreet") val userStreet: String,
+    @NonNull @ColumnInfo(name = "userExtNum") val userExtNum: String,
     @ColumnInfo(name = "userIntNum") val userIntNum: String,
-    @ColumnInfo(name = "userCountry") val userCountry: String,
-    @ColumnInfo(name = "userState") val userState: String,
-    @ColumnInfo(name = "userTown") val userTown: String
+    @NonNull @ColumnInfo(name = "userCountry") val userCountry: String,
+    @NonNull @ColumnInfo(name = "userState") val userState: String,
+    @NonNull @ColumnInfo(name = "userTown") val userTown: String
 ) {
 }
